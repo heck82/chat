4090824bf99b74965e8df001b8ffc60b918510d0
heck82 <heck82@meta.ua>
2018-09-07 17:55:25 +0300

    files modified

--- cliant.js
+++ cliant.js
@@ -1,74 +1,44 @@
 'use strict'
 let net = require('net')
+let readline = require('readline')
+let rl = readline.createInterface(process.stdin, process.stdout)
 require('ansicolor').nice
-
 if(process.argv.length < 3){
 	console.error('please add port as argument'.bright.red)
 	process.exit()
 }
-
 let n = true
 let obj = {}
-
-
 let client = net.connect(process.argv[2], (err)=>{
 	//	console.log('connected')
 	if (err) throw err	
 })
-
-console.log('**WELCOME to '+'CHANCK'.bright.magenta+'-Chat !!**')
-console.log('plase enter your name:')
-
-process.stdin.on("data", (data)=>{
-	let txt = data.toString().trim()
-	if(n == true && txt !=""|" "){
-		obj.name = txt
+console.log('**  WELCOME to CHANCK-Chat !!  **'.bgGreen.bright.yellow)
+//console.log('plase enter your name:')
+rl.question('Please enter your nick: ', (name)=>{
+	obj.name = name
+	if (name){
 		client.write(JSON.stringify(obj))
-		n = false
-		//		console.log('entering name')
-	}else if(txt != ""|" "){
-		obj.msg = txt
-		client.write(JSON.stringify(obj))
-		//		console.log('is a msg')
 	}
+	rl.close()
 })
-
+//process.stdin.on("data", (data)=>{
+//	let txt = data.toString().trim()
+//	if(n == true && txt !=""|" "){
+//		obj.name = txt
+//		client.write(JSON.stringify(obj))
+//		n = false
+//	}else if(txt != ""|" "){
+//		obj.msg = txt
+//		client.write(JSON.stringify(obj))
+//	}
+//})
 client.on('data', (data)=>{
 	if(obj.name){
 		console.log(data.toString())
 	}
 })
-
 client.on('end', ()=>{
 	console.log("connection interrupted".bgRed.white+"\nPlease reconnect".bgGreen.white)
 	process.exit()
 })
-
-
-
-
-//	if (c == 0){
-//		return client.write(`{"type" : "name", "name" : "${txt}"}`)
-//	}else{
-//		return client.write(`{"type" : "msg", "msg" : "${txt}"}`)
-//	}
-//	c ++
-//	process.exit()
-//})
-//let stream = thr2(verify)
-//function verify(chunk, _, next){
-//	if (n == true){
-//		name = chunk.toString().trim()
-//		this.push('your name is '+chunk.toString().trim())
-//		next()
-//		n = false
-//	}else{
-//		this.push(name+' say '+chunk.toString().trim())
-//	}
-//}
-//function end(done){
-//	done()
-//}
-//process.stdin.pipe(stream).pipe(client)
-//client.write("haha")
-
--- sarver.js
+++ sarver.js
@@ -5,47 +5,19 @@ let clients = []
 let server = net.createServer((socket)=>{
 	socket.id = socket.remoteAddress+":"+socket.remotePort
 	clients.push(socket)
-	//	greating(socket, (name)=>{
-	//		socket.name = name
-	//		clients.forEach((client)=>{
-	//			if (client != socket)
-	//			client.write(`${socket.name} has join to chat.`)
-	//		})
-	//		return
-	//	})
-	//	function greating (socket, cb){
-	//		socket.write("Welcome in chat!\nPlease enter your name:")
-	//		socket.on("data", (data)=>{
-	//			return cb(data.toString().trim("\n"))
-	//		})
-	//		return
-	//	}
 	socket.on('end', ()=>{
-		console.log(`${socket.id}(${socket.name}) has been disconected`)
+		console.log(`${socket.name} has been disconected`)
 		socket.destroy()
 		clients.splice(clients.indexOf(socket), 1)
 		clients.forEach((client)=>{
 			client.write(socket.name.green+" has left a chat.".bright.cyan)
 		})
 	})
-
 	socket.on("data", (data)=>{
 		let obj = JSON.parse(data.toString().trim())
-		//		console.log(data.toString())
-		//		socket.write(data.toString())
-		////		if (obj.type == "name"){
-		////			socket.name = obj.name
-		////			console.log(socket.name)
-		////		}else{
-		////		broadcast(obj.msg, socket)
-		////		}
-
 		socket.name = obj.name
 		broadcast(obj, socket)
-
-		//		console.log(data.toString())
 	})
-
 	function broadcast (obj, sender){
 		clients.forEach((client)=>{
 			if(!obj.msg){
